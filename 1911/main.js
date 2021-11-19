f19a808461a7abd898af01c55e8338876711346c
sdaurenbekova04 <92543476+sdaurenbekova04@users.noreply.github.com>
2021-11-19 13:55:50 +0600

    Add files via upload

---
+++ 1911/main.js
@@ -0,0 +1,24 @@
+function test1() {
+    let name = prompt('Введите имя')
+    document.getElementById('name').textContent = 'Имя: ' + name
+}
+function test2() {
+    let surname = prompt('Введите фамилию')
+    document.getElementById('surname').textContent = 'Фамилия: ' + surname
+}
+function test3() {
+    let age = prompt('Введите возраст')
+    document.getElementById('age').textContent = 'Возраст: ' + age
+}
+function test4() {
+    let prof = prompt('Введите специальность')
+    document.getElementById('prof').textContent = 'Специальность: ' + prof
+}
+function test5() {
+    let date = prompt('Введите дату рождения')
+    document.getElementById('date').textContent = 'Дата рождения: ' + date
+}
+function test6() {
+    let about = prompt('Введите о себе')
+    document.getElementById('about').textContent = 'О себе: ' + about
+}
